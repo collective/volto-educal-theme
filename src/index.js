c236b930fe2c5e06108b0f8d6333bfb0a209e332
avimishra18 <mishra.avi18@gmail.com>
2022-08-21 23:48:19 +0530

    🚧 Added eventListing boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import {
+  BannerAreaBlockEdit,
+  BannerAreaBlockView,
+} from '@plone-collective/volto-educal-theme/components/Blocks/BannerAreaBlock';
+import {
   CategoryTeaserEdit,
   CategoryTeaserView,
 } from '@plone-collective/volto-educal-theme/components/Blocks/CategoryTeaser';
 import {
-  BannerAreaBlockEdit,
-  BannerAreaBlockView,
-} from '@plone-collective/volto-educal-theme/components/Blocks/BannerAreaBlock';
+  CourseBlockEdit,
+  CourseBlockView,
+} from '@plone-collective/volto-educal-theme/components/Blocks/CourseBlock';
+import EducalEventListing from '@plone-collective/volto-educal-theme/components/Blocks/Listing/EducalEventListing';
 
 import categoryTeaserIcon from '@plone/volto/icons/freedom.svg';
 import bannerAreaBlockIcon from '@plone/volto/icons/content-existing.svg';
+import courseBlockIcon from '@plone/volto/icons/content-existing.svg';
 
 import '@plone-collective/volto-educal-theme/slate-inlineStyles.less';
 import '@plone-collective/volto-educal-theme/tabStyles.less';
@@ -48,12 +54,29 @@ const applyConfig = (config) => {
       view: [],
     },
   };
+  config.blocks.blocksConfig.educalCourseBlock = {
+    id: 'educalCourseBlock',
+    title: 'Educal Course Block',
+    icon: courseBlockIcon,
+    group: 'common',
+    view: CourseBlockView,
+    edit: CourseBlockEdit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+    enableStyling: true,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+  };
   // Adding teaserBlocks to Grid
   config.blocks.blocksConfig.__grid.gridAllowedBlocks = [
     ...config.blocks.blocksConfig.__grid.gridAllowedBlocks,
     'slate',
-    'educalCategoryTeaser',
     'educalBannerAreaBlock',
+    'educalCategoryTeaser',
+    'educalCourseBlock',
   ];
   // Slate Config
   config.settings.slate.styleMenu.inlineStyles = [
@@ -65,6 +88,15 @@ const applyConfig = (config) => {
     {
       cssClass: 'educalRightLink',
       label: 'Educal Right Text',
+    },
+  ];
+  // Add event listing
+  config.blocks.blocksConfig.listing.variations = [
+    ...config.blocks.blocksConfig.listing.variations,
+    {
+      id: 'educalEvent',
+      title: 'Educal Events',
+      template: EducalEventListing,
     },
   ];
   return config;
